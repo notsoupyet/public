6d4f5d45f1d0d8cc9341f9c4fbe2d57319f64e46
Alex Wang <alex@notsoupyet.com>
2020-02-27 00:50:46 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    title: `Not Soup Yet`,
+    name: `Alex W.`,
+    siteUrl: `https://notsoupyet.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
       heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
